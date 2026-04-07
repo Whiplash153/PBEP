fa97b8a21c4028387decf3938109455a957015c4
Mikhail Mashenistov <pr.mashinistov@gmail.com>
2026-04-07 14:09:15 +0500

    Approval app: proposal_repo

--- Programs/03_approval/app/repositories/proposal_repository.py
+++ Programs/03_approval/app/repositories/proposal_repository.py
@@ -1,2 +1,16 @@
+from sqlalchemy.orm import Session
+from app.models.proposal import Proposal
+
 class ProposalRepo:
-    def __init__(self, ):
+    def __init__(self, session: Session):
+        self.session = session
+
+    def add(self, proposal: Proposal):
+        self.session.add(proposal)
+
+    def get_by_id(self, proposal_id):
+        result = self.session.query(Proposal).filter(Proposal.id == proposal_id).first()
+        return result
+
+    def delete(self, proposal):
+        self.session.delete(proposal)
