a26241070a6abe21e903ba89f9f81e7aec327ea4
Mikhail Mashenistov <pr.mashinistov@gmail.com>
2026-07-01 17:13:25 +0500

    Stage 8: docker into main and test app

--- Programs/03_approval/alembic/env.py
+++ Programs/03_approval/alembic/env.py
@@ -1,3 +1,7 @@
+import os
+
+from dotenv import load_dotenv
+
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -11,6 +15,14 @@ import app.models
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+load_dotenv()
+
+database_url = os.getenv("DATABASE_URL")
+if not database_url:
+    raise RuntimeError("DATABASE_URL is not set")
+
+config.set_main_option("sqlalchemy.url", database_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
--- Programs/03_approval/app/db/session.py
+++ Programs/03_approval/app/db/session.py
@@ -1,7 +1,14 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql+psycopg2://mikhailshelby@localhost:5432/approve_db"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL is not set")
 
 engine = create_engine(
     DATABASE_URL,
@@ -14,10 +21,10 @@ SessionLocal = sessionmaker(
     autocommit=False
 )
 
+
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
-
